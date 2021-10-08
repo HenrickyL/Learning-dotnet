e75fa158f92015aa9f9236c50c0c9fa167805143
HenrickyL <henrickyl1@gmail.com>
2021-10-07 23:08:54 -0300

    update

---
+++ CSharp/DIO - Primeiros Passos/1_DIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int numSteps = 5;
+            for(int i=0; i< numSteps; i++){
+                Console.WriteLine($"step: {i}");
+            }
+        }
+    }
+}
